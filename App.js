41adf2d535e15822932ee93d6c758da9f77828d5
Matthieu LAURENT <matthieulaurent@MacBook-Pro-de-coding.local>
2023-11-24 02:03:06 +0100

    subscribe page + start google login

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './pages/home'
+import Sub from './pages/subscribe'
 
 export default function App() {
   return (
-    <Home/>
+    <Sub/>
   );
 }
 
