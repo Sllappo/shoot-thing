42d078f3dcd7bc0daf7e8460300f9fb345438545
Vanessa <vanessa.boissierpro@gmail.com>
2023-12-22 09:09:28 +0100

    Ajout de la cam

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import Users from './pages/users';
 import Sub from './pages/subscribe';
 import profiledit from './pages/profiledit';
 import { NavigationContainer } from '@react-navigation/native';
+import camera from './pages/camera';
 const Stack = createNativeStackNavigator()
 
 export default function App() {
@@ -13,11 +14,12 @@ export default function App() {
     // <Home/>
 
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Profiledit">
+      <Stack.Navigator initialRouteName="Camera">
         <Stack.Screen name="Home" options={{ headerShown: false }} component={Home} />
-        {/* <Stack.Screen name="Users" options={{headerShown:false}} component={Users}/> */}
-        {/* <Stack.Screen name="Sub" options={{headerShown:false}} component={Sub}/> */}
-        {/* <Stack.Screen name="Profiledit" options={{headerShown:false}} component={profiledit}/> */}
+        <Stack.Screen name="Users" options={{headerShown:false}} component={Users}/>
+        <Stack.Screen name="Sub" options={{headerShown:false}} component={Sub}/>
+        <Stack.Screen name="Profiledit" options={{headerShown:false}} component={profiledit}/>
+        <Stack.Screen name="Camera" options={{headerShown:false}} component={camera}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
