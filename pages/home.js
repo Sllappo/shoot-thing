39896866d2dfa548e2acbdf0fccffda76c96f188
Vanessa <vanessa.boissierpro@gmail.com>
2024-04-05 09:35:50 +0200

    upload photo

--- pages/home.js
+++ pages/home.js
@@ -3,133 +3,49 @@ import Users from './users';
 import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { NavigationContainer } from '@react-navigation/native';
-import { Camera } from 'expo-camera';
 
-export default function Home({ navigation }) {
-    const [hasAudioPermission, setHasAudioPermission] = useState(null);
-    const [hasCameraPermission, setHasCameraPermission] = useState(null);
-    const [camera, setCamera] = useState(null);
-    const [record, setRecord] = useState(null);
-    const [type, setType] = useState(Camera.Constants.Type.back);
+import { supabase } from 'supabase.js';
+
+const supabaseUrl = 'https://isdrblvcgfkosdxpmetw.supabase.co/';
+const supabaseKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzZHJibHZjZ2Zrb3NkeHBtZXR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxMzQ0NTMsImV4cCI6MjAyNzcxMDQ1M30.zmJEXSatrtQMb_7Gmnh5SuxDO_z2uKYvM7pMLHMlcug';
 
 
-    useEffect(() => {
-        (async () => {
-            const cameraStatus = await Camera.requestCameraPermissionsAsync();
-            setHasCameraPermission(cameraStatus.status === 'granted');
-            const audioStatus = await Camera.requestMicrophonePermissionsAsync();
-            setHasAudioPermission(audioStatus.status === 'granted');
-        })();
-    }, []);
 
-    const takeVideo = async () => {
-        if (camera) {
-            const data = await camera.recordAsync()
-            setRecord(data.uri);
-            console.log(data.uri);
+
+export default function ImageUploadScreen() {
+    const [image, setImage] = useState(null);
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes:
+                ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+        if (!result.cancelled) {
+            setImage(result.uri)
         }
-    }
+    };
 
-    const stopVideo = async () => {
-        camera.stopRecording();
-    }
+    const uploadImage = async () => {
+        const file = await fetch(image);
+        const blob = await file.blob();
+        const { data, error } = await supabase.storage.from('images').upload((`image-${Date.now()}`, blob));
 
-    return (
-        <View style={styles.container}>
-            <View style={{
-                flex: 1, borderColor: "red", borderWidth: 2, height: "100%", width: "100%"
-            }}>
-                <Camera
-                    ref={ref => setCamera(ref)}
-                    style={{
-                        flex: 1,
-                    }}
-                    type={type}
-                    ratio={'4:3'} />
-            </View>
-            <Button
-                title="Flip Video"
-                onPress={() => {
-                    setType(
-                        type === Camera.Constants.Type.back
-                            ? Camera.Constants.Type.front
-                            : Camera.Constants.Type.back
-                    );
-                }}>
-            </Button>
-            <Button title="Take video" onPress={() => takeVideo()} />
-            <Button title="Stop Video" onPress={() => stopVideo()} />
-            {/* <View style={{ justifyContent: "center", height: "70%", width: 245, }}>
-                <Text style={styles.text}>
-                    Shoot Thing
-                </Text>
-                <View style={styles.containerhome}>
-                    <Pressable style={styles.submit} onPress={() => navigation.replace('Users')}>
-                        <Text style={styles.button}>CONNEXION</Text>
-                    </Pressable>
-                    <Pressable style={styles.submit2} onPress={() => navigation.replace('Sub')}>
-                        <Text style={styles.button}>INSCRIPTION</Text>
-                    </Pressable>
-                </View>
-            </View> */}
-        </View>
-    );
+        if (error) {
+            Alert.alert('Error', 'Failed to upload image');
+        }
+        else {
+            Alert.alert('Success Image uploaded successfully');
+            //Enregistrer l'URL de l'image dans la base de donnée
+        }
+    };
+    (<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}> 
+    {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />} 
+    <Button title="Choisir une image" onPress={pickImage} /> 
+    <Button title="Uploader l'image" onPress={uploadImage} /> 
+    </View>)
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    containerhome: {
 
-        bottom: 0,
-        position: "absolute",
-    },
-    text: {
-        fontSize: 32,
-        justifyContent: 'center',
-        textAlign: 'center',
-        color: "#7D2AE7",
-        fontWeight: '900',
 
-    },
-    backgroundVideo: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-    },
-    submit: {
-        width: 245,
-        height: 63,
-        flexShrink: 0,
-        borderRadius: 36,
-        borderWidth: 4,
-        borderColor: "#FFF",
-        backgroundColor: "#A05BE34D",
-        justifyContent: "center",
-        marginTop: 20,
-    },
-    submit2: {
-        width: 245,
-        height: 63,
-        flexShrink: 0,
-        borderRadius: 36,
-        borderWidth: 4,
-        borderColor: "#FFF",
-        backgroundColor: "#C9B9D93D",
-        justifyContent: "center",
-        marginTop: 20,
-
-    },
-    button: {
-        color: "#FFF",
-        textAlign: "center",
-        fontSize: 20,
-        fontWeight: "800",
-    },
-});
